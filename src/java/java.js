4f2111fa19cbdff46153595ba5eb89e20cb5b92d
Henry <Hdanie031499@gmail.com>
2022-06-23 16:47:28 -0500

    Creación de src para ordenar hojas de styles y js

---
+++ src/java/java.js
@@ -0,0 +1,9 @@
+// let burger = document.querySelector('.fa-bars');
+// let menu = document.querySelector('.menu');
+
+// burger.addEventListener('click', () => {
+//     menu.classList.toggle("active");
+// }, false)
+// registro
+let container = document.querySelector('.container');
+console.log(container.clientHeight);
